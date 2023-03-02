2ead94ea8450accd4d98f2fb8594f62a0bbfbb2f
Louis <llb.proton@pm.me>
2023-03-02 08:03:20 +0100

    Added new theme apple

--- polybar/scripts/launch.sh
+++ polybar/scripts/launch.sh
@@ -8,8 +8,9 @@ killall -q polybar
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar_left.log /tmp/polybar_center.log /tmp/polybar_right.log
-polybar left 2>&1 | tee -a /tmp/polybar_left.log & disown
-polybar center 2>&1 | tee -a /tmp/polybar_center.log & disown
-polybar right 2>&1 | tee -a /tmp/polybar_right.log & disown
+# polybar left 2>&1 | tee -a /tmp/polybar_left.log & disown
+# polybar center 2>&1 | tee -a /tmp/polybar_center.log & disown
+# polybar right 2>&1 | tee -a /tmp/polybar_right.log & disown
 
+polybar base 2>&1 | tee -a /tmp/polybar_base.log & disown
 echo "Bars launched..."
