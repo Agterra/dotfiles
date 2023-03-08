eaa014dec522152f2fe10f0f75dd6cc2103d4b74
Louis <llb.proton@pm.me>
2023-03-08 09:27:00 +0100

    Updated screens script

--- i3/scripts/auto-display.sh
+++ i3/scripts/auto-display.sh
@@ -5,13 +5,13 @@
 IS_CLOSED=$(grep closed -a /proc/acpi/button/lid/LID0/state)
 
 # Retrieve all connected screens
-SCREENS=$(xrandr | grep \ connected | awk '{print substr($0, 0, 5)}')
+SCREENS=$(xrandr | grep \ connected | cut -d " " -f1)
 
 # Building xrandr command
 COMMAND="xrandr"
 
 # Defining primary display, would be the laptop
-MAIN=$(xrandr | grep primary | awk '{print substr($0, 0, 5)}')
+MAIN=$(xrandr | grep primary | cut -d " " -f1)
 
 # Building displays, depending on the laptop lid state
 for i in $SCREENS; do
